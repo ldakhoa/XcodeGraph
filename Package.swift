454513dd3e564a50131ff39fe47f93366180dbcb
Khoa Le <ldakhoa1308@gmail.com>
2023-08-23 13:24:41 +0700

    Setup Project

--- Package.swift
+++ Package.swift
@@ -1,23 +1,48 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "XcodeGraph",
+    platforms: [
+        .macOS(.v12)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .executable(
+            name: "xcodegraph",
+            targets: ["xcodegraph"]),
         .library(
-            name: "XcodeGraph",
-            targets: ["XcodeGraph"]),
+            name: "XcodeGraphKit",
+            targets: ["XcodeGraphKit"]),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/apple/swift-argument-parser.git",
+            .upToNextMinor(from: "1.1.4")),
+        .package(
+            url: "https://github.com/apple/swift-log.git",
+            .upToNextMinor(from: "1.4.2")),
+        .package(
+            url: "https://github.com/onevcat/Rainbow",
+            .upToNextMinor(from: "4.0.1")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "xcodegraph",
+            dependencies: [
+                .target(name: "XcodeGraphKit"),
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]),
         .target(
-            name: "XcodeGraph"),
+            name: "XcodeGraphKit",
+            dependencies: [
+                .product(name: "Logging", package: "swift-log"),
+                .product(name: "Rainbow", package: "Rainbow"),
+            ]
+        ),
         .testTarget(
-            name: "XcodeGraphTests",
-            dependencies: ["XcodeGraph"]),
+            name: "XcodeGraphKitTests",
+            dependencies: ["XcodeGraphKit"]),
     ]
 )
---
+++ Sources/XcodeGraphKit/Logger.swift
@@ -0,0 +1,33 @@
+import Foundation
+import Logging
+import Rainbow
+
+private(set) var logger = Logger(label: "com.ldakhoa.XcodeGraph")
+
+extension Logger {
+    func error(_ error: Error) {
+        if let error = error as? LocalizedError, let description = error.errorDescription {
+            self.error("❌ \(description)", metadata: .color(.red))
+        } else {
+            self.error("❌ \(error.localizedDescription)", metadata: .color(.red))
+        }
+    }
+
+    func error(_ error: String) {
+        self.error("❌ \(error)", metadata: .color(.red))
+    }
+}
+
+// MARK: - Helpers
+
+extension Logger.MetadataValue {
+    static func color(_ color: NamedColor) -> Self {
+        .stringConvertible(color.rawValue)
+    }
+}
+
+extension Logger.Metadata {
+    static func color(_ color: NamedColor) -> Self {
+        ["color": .color(color)]
+    }
+}
